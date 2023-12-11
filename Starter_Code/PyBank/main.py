768fa700e563534c61f94fa426ebac2b19591642
tshreenidhi <sthotad@homeoffice.wal-mart.com>
2023-12-11 14:33:12 -0800

    modified the code during tutoring session

--- Starter_Code/PyBank/main.py
+++ Starter_Code/PyBank/main.py
@@ -20,45 +20,56 @@ greatest_increase = ["", 0]
 revenue_change_list = []
 revenue_average = 0
 
+flag = True
+
+months = []
 
 #open and read the csv file and also reading it as a dictionary
 with open(budget_data_csv) as csv_file:  
-    csv_reader = csv.DictReader(csv_file, delimiter=",")
-
+    #csv_reader = csv.DictReader(csv_file, delimiter=",")
+    csv_reader = csv.reader(csv_file)
+    #skip the header row
+    header = next(csv_reader)
+    
     #Find the total months in the dataset
     for row in csv_reader:
 
         #Count the total of months in the dataset
         total_months += 1
 
-        #Calculate the total revenue over the entire period
-        total_revenue = total_revenue + int(row["Profit/Losses"])
+        months.append(row[0])       
 
-        #Calculate the average change in revenue between months over the entire period
-        revenue_change = float(row["Profit/Losses"])- previous_revenue
-        previous_revenue = float(row["Profit/Losses"])
-        revenue_change_list = revenue_change_list + [revenue_change]
-        month_of_change = [month_of_change] + [row["Date"]]
-       
+        total_revenue = total_revenue + int(row[1])
 
-        #ThFind the greatest revenue increase (date and amount) over the entire period
-        if revenue_change>greatest_increase[1]:
-            greatest_increase[1]= revenue_change
-            greatest_increase[0] = row['Date']
+        if flag == True:
+            prev = int(row[1])
+            flag = False
+        
 
-        #Find the greatest revenue decrease (date and amount) over the entire period
-        if revenue_change<greatest_decrease[1]:
-            greatest_decrease[1]= revenue_change
-            greatest_decrease[0] = row['Date']
-    #Average revenue is sum by length
-    revenue_average = sum(revenue_change_list)/len(revenue_change_list)
-
+        change = int(row[1]) - prev 
+        month_of_change.append(change)
+        prev = int(row[1])
+    
+    #first month will have 0 value. so delete that value
+    month_of_change.pop(0)
+    months.pop(0)
+    revenue_average = sum(month_of_change) / len(month_of_change)
+      
+    print(f'Total Months: {total_months} \n')
+    print(f'Total Revenue: ${total_revenue} \n')
+    print(f'Average Revenue Change ${revenue_average:.2f}\n')
+    print(f'Greatest Increase in Revenue: {months[month_of_change.index(max(month_of_change))]} (${max(month_of_change)})')
+    print(f'Greatest Decrease in Revenue: {months[month_of_change.index(min(month_of_change))]} (${min(month_of_change)})')
+    
 #write the output to a txt file
 with open(Output_path, 'w') as file:
     file.write("Financial Analysis\n")
     file.write("---------------------\n")
     file.write("Total Months: %d\n" % total_months)
     file.write("Total Revenue: $%d\n" % total_revenue)
-    file.write("Average Revenue Change $%d\n" % revenue_average)
-    file.write("Greatest Increase in Revenue: %s ($%s)\n" % (greatest_increase[0], greatest_increase[1]))
-    file.write("Greatest Decrease in Revenue: %s ($%s)\n" % (greatest_decrease[0], greatest_decrease[1]))
+    file.write(f'Average Revenue Change ${revenue_average:.2f}\n')
+    file.write(f'Greatest Increase in Revenue: {months[month_of_change.index(max(month_of_change))]} (${max(month_of_change)})\n')
+    file.write(f'Greatest Decrease in Revenue: {months[month_of_change.index(min(month_of_change))]} (${min(month_of_change)})')
+
+
+    
